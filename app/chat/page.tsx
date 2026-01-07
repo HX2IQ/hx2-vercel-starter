1038cbe0744b35b130b93d23310952aff85050cd
HX2IQ <ezdetailer@gmail.com>
2026-01-06 21:51:45 -0500

    Force dynamic /chat (no edge cache)

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 "use client";
 
 import { useState } from "react";
@@ -78,6 +81,7 @@ export default function ChatPage() {
     </div>
   );
 }
+
 
 
 
