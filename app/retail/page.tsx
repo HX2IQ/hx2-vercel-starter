f104ff0935ccf94595d963e70a1fb467c2a828ee
HX2IQ <ezdetailer@gmail.com>
2026-01-21 20:04:16 -0500

    Retail: force-dynamic to avoid stale caching

--- app/retail/page.tsx
+++ app/retail/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+
+export const dynamic = "force-dynamic";
 import Link from "next/link";
 import Image from "next/image";
 import { motion } from "framer-motion";
@@ -187,5 +189,6 @@ export default function RetailHome() {
     </main>
   );
 }
+
 
 
