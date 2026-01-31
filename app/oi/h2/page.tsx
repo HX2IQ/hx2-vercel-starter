fef93cd23ef37f87d8069890982c6435cc89bc06
HX2IQ <ezdetailer@gmail.com>
2026-01-31 10:34:37 -0500

    Fix /oi/h2 prerender: disable static export + force-dynamic revalidate=0

--- app/oi/h2/page.tsx
+++ app/oi/h2/page.tsx
@@ -1,8 +1,6 @@
-"use client";
-
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
-
+"use client";
 
 import React, { useMemo, useState } from "react";
 
@@ -176,4 +174,5 @@ export default function H2Page() {
     </div>
   );
 }
+
 
